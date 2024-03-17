4f78e0dd7b521e57e453718b6682a1e8a895755a
Nena <nevena.dabic@yahoo.com>
2024-03-17 11:33:50 -0500

    forloopadded

--- src/test/java/git_intro/FirstGit.java
+++ src/test/java/git_intro/FirstGit.java
@@ -5,6 +5,11 @@ public class FirstGit {
         System.out.println("My first  git practice,Nevena.");
         System.out.println("My second git push,Nevena");
         System.out.println("My branch Nena_feature is push");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            System.out.println("some code from branch Nena");
+        }
     }
 
 }
