980b292e167371d3306b6af8e87f062703e1dfdc
Nena <nevena.dabic@yahoo.com>
2024-03-17 11:25:13 -0500

    test3

--- src/test/java/git_intro/FirstGit.java
+++ src/test/java/git_intro/FirstGit.java
@@ -4,7 +4,7 @@ public class FirstGit {
     public static void main(String[] args){
         System.out.println("My first  git practice,Nevena.");
         System.out.println("My second git push,Nevena");
-        System.out.println("My branch Nena_feature is push");git
+        System.out.println("My branch Nena_feature is push");
     }
 
 }
